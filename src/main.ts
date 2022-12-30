3d524dfd7a9c3227d390094f3ba2d5486e58a56e
Dinesh Tamang <dineshtamang7263@gmail.com>
2022-12-31 01:03:38 +0530

    added express, morgan, mongoose and dotenv packages

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import * as dotenv from "dotenv"
+dotenv.config();
 import express from "express"
 import morgan from "morgan"
+import mongoose from "mongoose";
 
 const app = express()
 
@@ -7,9 +10,23 @@ const app = express()
 app.use(express.json());
 app.use(morgan('dev'));
 
+const start = async () => {
+    mongoose.set('strictQuery', true);
+    if(!process.env.MONGO_URI) throw new Error("MONGO_URI is required");    
+    try {
+       await mongoose.connect(process.env.MONGO_URI);
+    } catch(err){
+        throw err
+    }
+    app.listen(5000, () => console.log("server is running on port 5000"))
+}
+
+start()
+
 app.get("/", (req, res) => {
-    res.send({username: "Dinesh Tamang"});
+    res.status(200).json({
+        username: "dineshtamang14",
+        email: "dineshtamang14@gmail.com",
+        isAdmin: true
+    })
 })
-
-
-app.listen(5000, () => console.log("server is running on port 5000"))
