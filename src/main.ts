7c40ee7357b941d746b7d7c6bf25adea9048285e
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-01-01 16:12:10 +0530

    changed port

--- src/main.ts
+++ src/main.ts
@@ -65,4 +65,4 @@ const start = async () => {
 
 start()
 
-app.listen(5000, () => console.log("server is running on port 5000"))
+app.listen(8080, () => console.log("server is running on port 5000"))
