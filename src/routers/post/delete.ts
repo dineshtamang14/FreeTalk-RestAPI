10355cb3e6181556607a5b10240c01a65f872fd7
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-01-01 14:26:16 +0530

    created update and delete routes

---
+++ src/routers/post/delete.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response, Router } from "express";
+import Post from "src/models/post";
+
+const router = Router();
+
+
+router.delete("/api/post/delete/:id", async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    if(!id){
+        const error = new Error("post id is required") as CustomError;
+        error.status = 404;
+        next(error);
+    }
+
+    try {
+        await Post.findOneAndRemove({ _id: id });
+    } catch (err) {
+        next(new Error("post cannot be deleted"));
+    }
+
+    res.status(201).json({ success: true })
+})
+
+export { router as deletePostRouter }
